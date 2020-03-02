090d4bca4255b7a8caf9515ea7188983e7f49f3e
Dmytro-sys <dmytroheshel@gmail.com>
2020-03-02 22:45:23 +0200

    Start new vue project

---
+++ src/views/Register.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="card auth-card">
+    <div class="card-content">
+      <span class="card-title">Домашня бухгалтерія</span>
+      <div class="input-field">
+        <input id="email" type="text">
+        <label for="email">Email</label>
+        <small class="helper-text invalid">Email</small>
+      </div>
+      <div class="input-field">
+        <input id="password" type="password" class="validate">
+        <label for="password">Пароль</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
+      <div class="input-field">
+        <input id="name" type="text" class="validate">
+        <label for="name">Ім'я</label>
+        <small class="helper-text invalid">Name</small>
+      </div>
+      <p>
+        <label>
+          <input type="checkbox" />
+          <span>З правилами згідний</span>
+        </label>
+      </p>
+    </div>
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Зареєстуватись
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Вже є аккаунт?
+        <a href="/">Ввійти!</a>
+      </p>
+    </div>
+  </form>
+</template>
