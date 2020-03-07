546907dd85e7c2b9c1c21571d297836554465c49
Dmytro-sys <dmytroheshel@gmail.com>
2020-03-07 21:23:02 +0200

    Add errors treatment

--- src/main.js
+++ src/main.js
@@ -4,17 +4,43 @@ import Vuelidate from 'vuelidate'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import firebase from 'firebase/app'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 import 'materialize-css'
+import 'firebase/auth'
+import 'firebase/database'
+
 
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyBxSKe6r5vSWOXxPOs6PYu4kjqqEnPExls",
+  authDomain: "my-vuecrm1.firebaseapp.com",
+  databaseURL: "https://my-vuecrm1.firebaseio.com",
+  projectId: "my-vuecrm1",
+  storageBucket: "my-vuecrm1.appspot.com",
+  messagingSenderId: "1028039408880",
+  appId: "1:1028039408880:web:07b73bb3fc5fb067d51635",
+  measurementId: "G-JQ6V96GTKS"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+
+})
+
+
---
+++ src/utils/messages.js
@@ -0,0 +1,6 @@
+export default {
+    'logout': 'Ви вийшли з системи',
+    'auth/user-not-found': 'Користувач не знайдений',
+    'auth/wrong-password': 'Не правильний пароль',
+    'auth/email-already-in-use': 'Такий Email вже є в системі'
+}
