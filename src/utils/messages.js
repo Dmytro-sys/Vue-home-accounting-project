8d8cf8c0c708bd476c313d3fd69929d936f608e2
Dmytro-sys <dmytroheshel@gmail.com>
2020-03-21 16:58:33 +0200

    Add planning, history functionality

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout': 'Ви вийшли з системи',
+    'login': 'Для початку портібно ввійти в систему',
     'auth/user-not-found': 'Користувач не знайдений',
     'auth/wrong-password': 'Не правильний пароль',
     'auth/email-already-in-use': 'Такий Email вже є в системі'
--- src/views/Detail-record.vue
+++ src/views/Detail-record.vue
@@ -1,25 +1,59 @@
 <template>
   <div>
-    <div>
+
+    <Loader v-if="loading"/>
+
+    <div v-else-if="record">
       <div class="breadcrumb-wrap">
-        <a href="/history" class="breadcrumb">Історія</a>
-        <a class="breadcrumb">
-          Розхід
+        <router-link to="/history" class="breadcrumb">Історія</router-link>
+        <a @click.prevent class="breadcrumb">
+          {{ record.type === 'income' ? 'Дохід' : 'Розхід'}}
         </a>
       </div>
       <div class="row">
         <div class="col s12 m6">
-          <div class="card red">
+          <div 
+          class="card"
+          :class="{
+            'green': record.type === 'income',
+            'red': record.type === 'outcome'
+          }">
             <div class="card-content white-text">
-              <p>Опис:</p>
-              <p>Сума:</p>
-              <p>Категорія:</p>
+              <p>Опис: {{record.description}}</p>
+              <p>Сума: {{record.amount | currency}}</p>
+              <p>Категорія: {{record.categoryName}}</p>
 
-              <small>12.12.12</small>
+              <small>{{record.date | date('datetime')}}</small>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <p class="center" v-else >Запис не знайдено.</p>
+
   </div>
 </template>
+
+<script>
+export default {
+  name: 'detail',
+  data: () => ({
+    record: null,
+    category: null,
+    loading: true
+  }),
+  async mounted() {
+    const id = this.$route.params.id
+    const record = await this.$store.dispatch('fetchRecordById', id)
+    const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+
+    this.loading = false
+  },
+}
+</script>
